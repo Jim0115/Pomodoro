c64b886bd2b4fdd09aaef36fdde8b84770d15e1a
Jim0115 <Jim0115@users.noreply.github.com>
2016-04-05 21:48:58 +0800

    Implement basic function.

--- Pomodoro/ViewController.m
+++ Pomodoro/ViewController.m
@@ -10,18 +10,50 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *timeLabel;
+@property (weak, nonatomic) IBOutlet UIButton *startButton;
+
+@property (nonatomic) NSUInteger time;
+
 @end
 
 @implementation ViewController
 
+const uint DEFAULT_TIME = 10;
+int count = 0;
+
 - (void)viewDidLoad {
   [super viewDidLoad];
+  self.timeLabel.hidden = YES;
+  self.timeLabel.text = @"";
+  self.time = DEFAULT_TIME;
   // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-  [super didReceiveMemoryWarning];
-  // Dispose of any resources that can be recreated.
+- (IBAction)startTimer:(UIButton *)sender {
+  sender.hidden = YES;
+  self.timeLabel.hidden = NO;
+  [self countdown:nil];
+  self.timeLabel.text = [NSString stringWithFormat:@"%lu", self.time];
+  NSTimer* timer = [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(countdown:) userInfo:nil repeats:true];
+  timer.tolerance = 0.1;
+}
+
+- (void)countdown:(NSTimer*)timer {
+//  NSLog(@"%d", count++);
+  self.timeLabel.text = [NSString stringWithFormat:@"%lu", self.time - 1];
+  self.time--;
+  NSLog(@"%lu", self.time);
+  if (self.time == 0) {
+    [timer invalidate];
+    [self countdownDidFinish];
+  }
+}
+
+- (void)countdownDidFinish {
+  self.time = DEFAULT_TIME;
+  self.startButton.hidden = NO;
+  self.timeLabel.hidden = YES;
 }
 
 @end
