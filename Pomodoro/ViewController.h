466dbcc722faa2ca23975522587bf9680135202f
Jim0115 <Jim0115@users.noreply.github.com>
2016-04-05 21:08:47 +0800

    Initial commit.

---
+++ Pomodoro/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Pomodoro
+//
+//  Created by 王仕杰 on 4/5/16.
+//  Copyright © 2016 王仕杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Pomodoro/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Pomodoro
+//
+//  Created by 王仕杰 on 4/5/16.
+//  Copyright © 2016 王仕杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
