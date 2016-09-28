e50ecf53d804b7115011e3724a44dffb8e89bccc
Jim0115 <Jim0115@users.noreply.github.com>
2016-09-28 22:21:19 +0800

    Refactory HistoryTVC with NSFetchedResultController.

--- Pomodoro/HistoryTableViewController.m
+++ Pomodoro/HistoryTableViewController.m
@@ -12,20 +12,42 @@
 #import "RecordCell.h"
 #import "Record.h"
 #import "DetailTableViewController.h"
-#import <CoreData/CoreData.h>
+
+@import CoreData;
 
 @interface HistoryTableViewController ()
 
 @property (nonatomic, readonly) NSManagedObjectContext* context;
-@property (nonatomic, readonly, copy) NSArray* records;
-@property (nonatomic, copy) NSArray* processedByDate;
+//@property (nonatomic, readonly, copy) NSArray* records;
+//@property (nonatomic, copy) NSArray* processedByDate;
+
+@property (nonatomic, readonly) NSFetchedResultsController* fetchedResultController;
 
 @property (nonatomic) id observer;
-
 
 @end
 
 @implementation HistoryTableViewController
+
+#pragma mark - properties
+
+@synthesize fetchedResultController = _fetchedResultController;
+
+- (NSFetchedResultsController *)fetchedResultController {
+  if (!_fetchedResultController) {
+    NSFetchRequest* request = [NSFetchRequest fetchRequestWithEntityName:@"Record"];
+    request.sortDescriptors = @[[NSSortDescriptor sortDescriptorWithKey:@"starttime"
+                                                            ascending:YES]];
+    
+    _fetchedResultController = [[NSFetchedResultsController alloc] initWithFetchRequest:request
+                                                                   managedObjectContext:self.context
+                                                                     sectionNameKeyPath:@"starttime"
+                                                                              cacheName:@"HISTORY_CACHE"];
+    
+    [_fetchedResultController performFetch:nil];
+  }
+  return _fetchedResultController;
+}
 
 #pragma mark - vc life cycle
 
@@ -61,46 +83,47 @@
   return ((AppDelegate*)[UIApplication sharedApplication].delegate).managedObjectContext;
 }
 
-- (NSArray *)records {
-  NSArray* array;
-  
-  NSFetchRequest* request = [[NSFetchRequest alloc] initWithEntityName:@"Record"];
-  request.sortDescriptors = @[[NSSortDescriptor sortDescriptorWithKey:@"date"
-                                                            ascending:false
-                                                             selector:@selector(compare:)]];
-  array = [self.context executeFetchRequest:request error:nil];
-  
-//  return [[array reverseObjectEnumerator] allObjects];
-  return array;
-}
+//- (NSArray *)records {
+//  NSArray* array;
+//  
+//  NSFetchRequest* request = [[NSFetchRequest alloc] initWithEntityName:@"Record"];
+//  request.sortDescriptors = @[[NSSortDescriptor sortDescriptorWithKey:@"date"
+//                                                            ascending:false
+//                                                             selector:@selector(compare:)]];
+//  array = [self.context executeFetchRequest:request error:nil];
+//  
+//  //  return [[array reverseObjectEnumerator] allObjects];
+//  return array;
+//}
 
-- (NSArray *)processedByDate {
-  NSMutableArray* dates = [[NSMutableArray alloc] init];
-  NSDateFormatter* formatter = [[NSDateFormatter alloc] init];
-  formatter.locale = [NSLocale autoupdatingCurrentLocale];
-  NSArray* records = self.records;
-  NSMutableArray* processed = [[NSMutableArray alloc] init];
-  formatter.dateFormat = @"yyyy MM dd";
-  for (id obj in records) {
-    if ([obj isKindOfClass: [Record class]]) {
-      Record* r = (Record *)obj;
-      NSString* s = [formatter stringFromDate:r.date];
-      NSUInteger index = [dates indexOfObject:s];
-      if (index > NSIntegerMax - 10) {
-        [dates addObject:s];
-        [processed addObject:[[NSMutableArray alloc] initWithObjects:r, nil]];
-      } else {
-        [((NSMutableArray *)processed[index]) addObject:r];
-      }
-    }
-  }
-  return processed;
-}
+//- (NSArray *)processedByDate {
+//  NSMutableArray* dates = [[NSMutableArray alloc] init];
+//  NSDateFormatter* formatter = [[NSDateFormatter alloc] init];
+//  formatter.locale = [NSLocale autoupdatingCurrentLocale];
+//  NSArray* records = self.records;
+//  NSMutableArray* processed = [[NSMutableArray alloc] init];
+//  formatter.dateFormat = @"yyyy MM dd";
+//  for (id obj in records) {
+//    if ([obj isKindOfClass: [Record class]]) {
+//      Record* r = (Record *)obj;
+//      NSString* s = [formatter stringFromDate:r.date];
+//      NSUInteger index = [dates indexOfObject:s];
+//      if (index > NSIntegerMax - 10) {
+//        [dates addObject:s];
+//        [processed addObject:[[NSMutableArray alloc] initWithObjects:r, nil]];
+//      } else {
+//        [((NSMutableArray *)processed[index]) addObject:r];
+//      }
+//    }
+//  }
+//  return processed;
+//}
 
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-  return self.processedByDate.count;
+//  return self.processedByDate.count;
+  return self.fetchedResultController.sections.count;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
@@ -120,69 +143,45 @@
   //  cell.textLabel.text = info;
   
   //  cell.times = self.records.count;
-  //  cell.times = [self.tableView numberOfRowsInSection:indexPath.section];
-  cell.times = ((NSArray *)self.processedByDate[indexPath.section]).count;
+  //  cell.times = [self.tableView numberOfRowsInSection:indexPath.section]
+  
+//  cell.times = ((NSArray *)self.processedByDate[indexPath.section]).count;
+  
+//  cell.times = [self.fetchedResultController objectAtIndexPath:indexPath];
+  
+  cell.times = self.fetchedResultController.sections.count;
   
   return cell;
 }
 
-- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
-  UILabel* header = [[UILabel alloc] init];
-  NSArray* currentMonthArray = (NSArray *)self.processedByDate[section];
-  Record* record = (Record *)currentMonthArray.firstObject;
-  NSDateFormatter* formatter = [[NSDateFormatter alloc] init];
-  formatter.locale = [NSLocale autoupdatingCurrentLocale];
-  //  formatter.dateFormat = @"   MMM dd, yyyy";
-  formatter.dateStyle = NSDateFormatterMediumStyle;
-  header.text = [formatter stringFromDate:record.date];
-  header.backgroundColor = [UIColor whiteColor];
-  return header;
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+//  return @"Title";
+  return self.fetchedResultController.sectionIndexTitles[section];
 }
 
-/*
- // Override to support conditional editing of the table view.
- - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
- // Return NO if you do not want the specified item to be editable.
- return YES;
- }
- */
-
-/*
- // Override to support editing the table view.
- - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
- if (editingStyle == UITableViewCellEditingStyleDelete) {
- // Delete the row from the data source
- [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
- } else if (editingStyle == UITableViewCellEditingStyleInsert) {
- // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
- }
- }
- */
-
-/*
- // Override to support rearranging the table view.
- - (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
- }
- */
-
-/*
- // Override to support conditional rearranging of the table view.
- - (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
- // Return NO if you do not want the item to be re-orderable.
- return YES;
- }
- */
+//- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
+//  UILabel* header = [[UILabel alloc] init];
+//  NSArray* currentMonthArray = (NSArray *)self.processedByDate[section];
+//  Record* record = (Record *)currentMonthArray.firstObject;
+//  NSDateFormatter* formatter = [[NSDateFormatter alloc] init];
+//  formatter.locale = [NSLocale autoupdatingCurrentLocale];
+//  //  formatter.dateFormat = @"   MMM dd, yyyy";
+//  formatter.dateStyle = NSDateFormatterMediumStyle;
+//  header.text = [formatter stringFromDate:record.date];
+//  header.backgroundColor = [UIColor whiteColor];
+//  return header;
+//}
 
 
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-  if ([segue.destinationViewController isKindOfClass:[DetailTableViewController class]]) {
-    DetailTableViewController* destination = segue.destinationViewController;
-    destination.daily = self.processedByDate[[self.tableView indexPathForCell:sender].section];
-  }
-}
+//- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+//  if ([segue.destinationViewController isKindOfClass:[DetailTableViewController class]]) {
+//    DetailTableViewController* destination = segue.destinationViewController;
+//    destination.daily = self.processedByDate[[self.tableView indexPathForCell:sender].section];
+//  }
+//}
 
 
 @end
